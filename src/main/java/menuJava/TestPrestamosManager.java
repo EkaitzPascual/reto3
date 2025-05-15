f284093d4a331fb40c7f2a84fb1ee6a25d9c2db3
Mikel <mikelgh06@gmail.com>
2025-05-15 23:11:53 +0200

    a

--- src/main/java/menuJava/TestPrestamosManager.java
+++ src/main/java/menuJava/TestPrestamosManager.java
@@ -5,29 +5,24 @@ import org.junit.Test;
 public class TestPrestamosManager {
     @Test
     public void testPrestamosManager() {
-        // Create an instance of PrestamosManager
         PrestamosManager prestamosManager = new PrestamosManager();
 
-        // Test the constructor
         assertNotNull(prestamosManager);
     }
 
     @Test
     public void testGetPrestamos() {
-        // Create an instance of PrestamosManager
         PrestamosManager prestamosManager = new PrestamosManager();
 
-        // Test the getPrestamos method
         assertNotNull(prestamosManager.getPrestamos());
     }
 
     @Test
     public void testAddPrestamo() {
-        // Create an instance of PrestamosManager
         PrestamosManager prestamosManager = new PrestamosManager();
 
-        // Test adding a new loan
         prestamosManager.addPrestamo("12345", "2023-10-01", "2023-10-15");
         assertEquals(1, prestamosManager.getPrestamos().size());
     }
 }
+
